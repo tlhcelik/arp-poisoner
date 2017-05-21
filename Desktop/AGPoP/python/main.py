b8df32c7d04c25baf9d7149f8f5f1b20f458565c
Talha Celik <tlhcelik81@gmail.com>
2017-05-21 22:50:12 +0300

    Add files via upload

--- Desktop/AGPoP/python/main.py
+++ Desktop/AGPoP/python/main.py
@@ -8,7 +8,7 @@ usage:
 from os import system as linux
 from os.path import exists 
 from os import geteuid
-
+from os import getcwd as pwd
 import sys
 from colors import COLORS 
 
@@ -26,14 +26,77 @@ def project(path):
 	else:
 		sys.exit()
 	
-	print(COLORS.magenta+"[*]For pushing file to commit write [Default (Via file upload) hit enter]"+COLORS.black)
+	current_path = copy_files(path)
+	
+	print(COLORS.magenta+"[*]For pushing file to commit write [Default (Add files via upload) hit enter]"+COLORS.black)
+	ans = raw_input()
+
+	if len(ans) == 0:
+		linux("git commit -m \"Add files via upload\" {0}".format(current_path))
+	else:
+		linux("git commit -m {0} {1}".format(str(ans),current_path))
+	
+	print(COLORS.blue+"[*]Write to GitHub repo URL [ https://github.com/username/repo_name.git ]."+COLORS.black)
+	url = raw_input()
+	
+	if(len(url) != 0):
+		add_url(url)
+
+	print(COLORS.blue+"[*]Write to branch [ Default master ]."+COLORS.black)
+	branch = raw_input()
+	print(COLORS.blue+"[*]Wait a second ..."+COLORS.black)
+	
+	if(len(branch) == 0):
+		branch = "master"
+		linux("git pull origin {0}".format(str(branch))) #a1
+	else:
+		linux("git pull origin {0}".format(str(branch))) #a2
+
+	print(COLORS.green+"[*]Everything is okey."+COLORS.black)
+	print(COLORS.blue+"[*]Do you push repo [Default (Y)/(N)]."+COLORS.black)
 	ans = raw_input()
 	
 	if len(ans) == 0:
-		linux("git commit -m \"Via File Upload\" {0}".format(path))
+		push_project(branch,url,current_path)
+		sys.exit() # Program finish
 	else:
-		linux("git commit -m {0}".format(str(ans)))
+		sys.exit()
+
+def copy_files(path):
+	current_path = pwd()
+	try:
+		linux("cp -rf {0} {1}".format(path,current_path))
+		return current_path
+	except:
+		sys.exit()
 		
+def push_project(branch,url,path):
+	try:
+		linux("git push origin {0}".format(str(branch)))
+		print(COLORS.green+"[*]File's uploaded."+COLORS.black)
+		print(COLORS.green+"[*]Goto project: "+url+""+COLORS.black)
+		sys.exit()
+	except:
+		print(COLORS.red+"[!]Push Error. Try Againing!"+COLORS.black)
+		try:
+			linux("git push origin {0} --force".format(str(branch)))
+			print(COLORS.green+"[*]File's uploaded."+COLORS.black)
+			print(COLORS.green+"[*]Goto project: "+url+""+COLORS.black)
+		except:
+			print(COLORS.red+"[!]Push Error. Exiting!"+COLORS.black)
+			print(COLORS.red+"[!]Manual try code : \n!"+COLORS.black)
+			print(COLORS.blue+"[!]git push origin <branch> --force"+COLORS.black)
+			sys.exit()
+				
+def add_url(url):
+	try:	
+		linux("git remote add origin {0}".format(url))
+		print(COLORS.green+"[*]Remote GitHub repo added."+COLORS.black)
+		
+	except:
+		print(COLORS.red+"[!]URL Error. Try Again!"+COLORS.black)
+		add_url(path)
+	pass	
 		
 def usage():
 	print("help")
